84227cce3cc7b9698ffbfc99715946db95fc2542
Mehdi <mehdi.naderi.64@gmail.com>
2025-10-02 17:10:27 +0330

    Add files via upload

---
+++ AlcoholCalculator/Services/MauiStorageService.cs
@@ -0,0 +1,28 @@
+﻿using AlcoholCalculator.Shared.Services;
+
+namespace AlcoholCalculator.Services;
+
+public class MauiStorageService : IStorageService
+{
+    public async Task<string> ReadFileAsync(string fileName)
+    {
+        var filePath = Path.Combine(FileSystem.AppDataDirectory, fileName);
+        if (File.Exists(filePath))
+        {
+            return await File.ReadAllTextAsync(filePath);
+        }
+        return string.Empty;
+    }
+
+    public async Task WriteFileAsync(string fileName, string content)
+    {
+        var filePath = Path.Combine(FileSystem.AppDataDirectory, fileName);
+        await File.WriteAllTextAsync(filePath, content);
+    }
+
+    public Task<bool> FileExistsAsync(string fileName)
+    {
+        var filePath = Path.Combine(FileSystem.AppDataDirectory, fileName);
+        return Task.FromResult(File.Exists(filePath));
+    }
+}
